f7ec434004af1087538a1eaf62bcd559515c3bcd
k21082681 <shahad.alhamazani@kcl.ac.uk>
2023-10-14 22:53:54 +0100

    Enforce constraints on Thing model (fixed)

--- things/models.py
+++ things/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.db.models import Model
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.TextField(max_length=120, unique=False, blank=True)
     quantity = models.IntegerField(unique=False, 
-                                   MinValueValidator= 0,
-                                   MaxValueValidator= 100,)
+                                   validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ])
