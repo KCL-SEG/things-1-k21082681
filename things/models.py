4bc6f2abe7e8a7a127f2228aba663f0fb18ff5ad
k21082681 <shahad.alhamazani@kcl.ac.uk>
2023-10-14 22:49:48 +0100

    Enforce constraints on Thing model

--- things/models.py
+++ things/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.db.models import Model
 
 class Thing(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.TextField(max_length=120, unique=False, blank=True)
+    quantity = models.IntegerField(unique=False, 
+                                   MinValueValidator= 0,
+                                   MaxValueValidator= 100,)
