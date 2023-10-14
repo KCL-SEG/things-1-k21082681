599719db5ac226c708e13aa40b441381bb092b8c
k21082681 <shahad.alhamazani@kcl.ac.uk>
2023-10-14 22:36:50 +0100

    Create a Thing model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+class Thing(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    quantity = models.IntegerField()
